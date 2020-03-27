c7dd899fc8c05c80efdd9ee118c4984363db94d7
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-03-27 21:52:42 +0100

    Remove old code.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React, { useState } from 'react';
 import './Circle.css';
 
 function App() {
-  const [animated, setAnimated] = useState(false)
   const [show, setShow] = useState(true);
-
-  const animate = () => {
-    setAnimated(!animated)
-    setTimeout(() => {
-      setShow(!show)
-    }, 1000)
-  }
 
   return (
     <div>
-      <button onClick={() => animate()}>animuj</button>
+      <button onClick={() => setShow(!show)}>animuj</button>
       {show && (
-        <div className={
-          `Circle ${animated ? 'Circle--animated' : ''}`
-        } />
+        <div
+          className="Circle"
+        />
       )}
     </div>
   );
