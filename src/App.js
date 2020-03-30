ef66915a2a985748dad07e40c7b89fc12c1a1810
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-03-30 23:29:13 +0200

    Animate list.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,84 @@
-import React, { useState } from 'react';
-import './Circle.css';
-import { motion, AnimatePresence } from 'framer-motion'
+import React, { useState } from "react";
+import "./Circle.css";
+import "./App.css";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
-  const [show, setShow] = useState(true);
+  const [newItem, setNewItem] = useState('');
+  const [items, setItems] = useState([
+    "Apple",
+    "Banana",
+    "Cherry",
+    "Orange",
+    "Kiwi",
+    "Pear"
+  ]);
+
+  const addNewItem = (e) => {
+    setItems([
+      ...items,
+      newItem,
+    ]);
+  };
+
+  const deleteItem = (itemToDelete) => {
+    setItems(items.filter(
+      (item) => item !== itemToDelete,
+    ));
+  };
 
   return (
-    <div>
-      <button onClick={() => setShow(!show)}>animuj</button>
+    <div className="App">
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        addNewItem();
+      }}>
+        <input
+          value={newItem}
+          className="input App__input"
+          type="text"
+          placeholder="Dodaj nowy owoc..."
+          onChange={(e) => {
+            setNewItem(e.target.value);
+          }}
+        />
+      </form>
       <AnimatePresence>
-        {show && (
-          <motion.div
-            className="Circle"
-            initial={{
-              y: 250,
-              scale: 0,
-            }}
-            animate={{
-              y: 50,
-              scale: 1,
-              transition: {
-                duration: 1,
-              },
-            }}
-            exit={{
-              y: 250,
-              scale: 0,
-              transition: {
-                duration: 1,
-              },
-            }}
-          />
-        )}
+        {items.map((item) => {
+          return (
+            <motion.div
+              key={item}
+              className="notification is-info"
+              positionTransition
+              initial={{
+                x: 50,
+                opacity: 0
+              }}
+              animate={{
+                x: 0,
+                opacity: 1,
+                transition: {
+                  duration: .4,
+                }
+              }}
+              exit={{
+                x: 50,
+                opacity: 0,
+                transition: {
+                  duration: .4,
+                }
+              }}
+            >
+              <button
+                className="delete"
+                onClick={() => {
+                  deleteItem(item);
+                }}
+              />
+              {item}
+            </motion.div>
+          );
+        })}
       </AnimatePresence>
     </div>
   );
